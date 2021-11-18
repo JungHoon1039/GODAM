f7c7cca7bbc9ffae97be0101ce490bcc3db50692
JungHoon <wjdgns1110@gmail.com>
2021-11-18 18:10:23 +0900

    aa

--- 3진법 뒤집기.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    answer = 0
-    lst = []
-    while (n>0):
-        lst.append(int(n%3))
-        n = n//3
-    for i in range(0,len(lst)): 
-        answer += lst[i] * 3**(len(lst)-(i+1))
-    return answer
