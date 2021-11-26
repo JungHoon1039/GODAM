fdda8c1778ea2a2ded53a7ea80ba40886f4425a3
JungHoon <wjdgns1110@gmail.com>
2021-11-26 18:52:24 +0900

    11.26 전체 팀원 작업
    정훈
    delete.html 삭제
    base2.html 로그아웃 팝업추가 model.py region 항목 db조사
    urls.py 수정
    view.py 통합 및 수정  현성 edit ->  비밀번호에서 회원정보 전체수정 view 작업중 model.py  region 항목 추가, city 작업중 info.html onclick 팝업추가  소민
    signup - 폰트위치 수정완료 / 반응형작업중 signup_com.html base 세팅 index.html base 세팅 login.html 타이틀 수정 base.html base2.html 수정
    
    은별님 와이어프레임 제작
    example.html 수정 info.html 수정  가연님 edit.html nav바 추가

--- GoDam/User/urls.py
+++ GoDam/User/urls.py
@@ -4,15 +4,13 @@ from . import views
 urlpatterns = [
         path('signup', views.signup),
         path('signup_com', views.signup_com),
+        path('check_id',views.check_id),
         path('login',views.login),
-        path('index', views.index),
         path('logged', views.logged),
         path('logout', views.logout),
-        path('check_id',views.check_id),
         path('info',views.userinfo),
-        path('edit',views.password_edit),
-        path('edit_com',views.password_edit_complete),
-        path('delete',views.member_delete),
         path('delete_com',views.member_delete_complete),
+        path('edit',views.info_edit),
+        path('edit_com',views.info_edit_complete),
 
 ]
