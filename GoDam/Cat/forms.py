64cce7c564f56feacfcae7c66eb2fbf5e2edacda
Hellodeu <hijin968@naver.com>
2021-12-01 15:08:07 +0900

    cat과 user 외래키 연동
    admin에서 캣 데이터 넣을ㄸㅐ User field 에는 User데이터를 선택 할 수 있고
    newcat 링크에서 고양이 등록 시 User값은 자동적으로 세션에 있는 로그인 유저로 데이터 저장됩니다

--- GoDam/Cat/forms.py
+++ GoDam/Cat/forms.py
@@ -1,7 +1,9 @@
 from django.forms import ModelForm
 from .models import Cat#Board
+from User.models import User
 
 class CatForm(ModelForm):
+
     class Meta:
         model = Cat
         fields = ['Catid','Catimage','Catname','Catage','Neutering','Region']
--- GoDam/Cat/views.py
+++ GoDam/Cat/views.py
@@ -7,14 +7,19 @@ from User.models import User
 # Create your views here.
 #cat create
 def catupload(req):
-    logged_member = User.objects.filter(Userid=req.session.get('Userid'))
     #post 방식으로 폼을 가져온 경우
     if req.method == "POST":
     #내용물 가져오기
+        logged_member = User.objects.get(Userid=req.session.get('Userid'))
         form = CatForm(req.POST,req.FILES)
     #내용물 존재 유무 유효성 검사,확인되면 db저장
         if form.is_valid():
-            form.save()
+            #form.save(commit=false) 의미는 폼의 내용물을 저장하는데 바로 db에 저장하지 않음,그러니까 완전히 저장하기 전에 사전작업이 있다는 의미
+            new_cat = form.save(commit=False)
+            #사전작업 = Cat.User 왜래필드를 User 세션에 투입
+            new_cat.User = logged_member
+            #이제 진짜 저장
+            new_cat.save()
             return redirect(catuploaded)
     #아닐 경우 submit 안됨
     else:
@@ -22,7 +27,7 @@ def catupload(req):
     return render(req,'upload.html', {'form': form})
 #만들어진거 확인
 def catuploaded(req):
-    #logged_member = User.objects.filter(Userid=req.session.get('Userid'))
+    logged_member = User.objects.filter(Userid=req.session.get('Userid'))
     return render(req,'uploaded.html')
 #모든 캣 보이게
 def catall(req):
@@ -31,7 +36,7 @@ def catall(req):
        return render(req,'catall.html',{'cat': cat,'logged_member':logged_member})
 #404 에러 기능으로 그 고양이 정보 가져오기
 def cat(req,Catid):
-       #logged_member = User.objects.filter(Userid=req.session.get('Userid'))
+       logged_member = User.objects.filter(Userid=req.session.get('Userid'))
        cat = get_object_or_404(Cat,pk=Catid)
        return render(req,'catcontent.html',{'i':cat})
 
