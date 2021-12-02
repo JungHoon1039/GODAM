48038cead4425f29c186841b94250f9b52bc1c3e
Hellodeu <hijin968@naver.com>
2021-12-02 18:23:34 +0900

    cat create 폼을catall page에 적용
    모달로 통해서 작성 폼 보이게 적용

--- GoDam/Cat/models.py
+++ GoDam/Cat/models.py
@@ -8,7 +8,7 @@ def _get_upload_to(instance, filename):
 
 class Cat(models.Model):
       Catid = models.AutoField(primary_key=True)
-      Catimage = models.ImageField(upload_to=_get_upload_to)
+      Catimage = models.ImageField(upload_to=_get_upload_to, verbose_name='사진')
       Catname = models.CharField(max_length=64, verbose_name='이름')
       Catage = models.IntegerField(verbose_name='나이')
       gender = (("수컷","수컷"),("암컷","암컷"))
@@ -19,7 +19,7 @@ class Cat(models.Model):
       region = (('강동구','강동구'),('송파구','송파구'),('강남구','강남구'),('서초구','서초구'),('관악구','관악구'),('동작구','동작구'),('금천구','금천구'),('영등포구','영등포구'),('구로구','구로구'),('양천구','양천구'),('강서구','강서구'),('마포구','마포구'),('은평구','은평구'),('서대문구','서대문구'),('종로구','종로구'),('중구','중구'),('용산구','용산구'),('강북구','강북구'),('성북구','성북구'),('동대문구','동대문구'),('성동구','성동구'),('도봉구','도봉구'),('노원구','노원구'),('중랑구','중랑구'),('광진구','광진구'))
       Region = models.CharField(max_length=16, verbose_name='지역',choices=region)
       Discribtion = models.TextField(verbose_name='설명')
-      User = models.ForeignKey("User.User", on_delete=models.CASCADE,db_column="user")
+      User = models.ForeignKey("User.User", on_delete=models.CASCADE,db_column="user",verbose_name='등록유저')
       Catupload = models.DateTimeField(auto_now_add=True, verbose_name='등록')
 
 
